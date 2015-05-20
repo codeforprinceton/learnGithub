fdd3bf5afab202687a77eee938961a9d506c6789
Edderic <edderic@gmail.com>
2015-05-20 14:00:02 -0400

    Update example.rb

--- example.rb
+++ example.rb
@@ -1,5 +1,5 @@
 puts 'hello world!'
 puts 'Hello the second time!'
-puts "change after someone's pull request"
+puts "change to create merge conflict"
 puts 'Hi from Batman'
 puts "Hi again from Batman in double quotes"
