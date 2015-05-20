62d76dd013950043ebfe92d536a19e063b19a5eb
Manu Kanthan <m.kanthan@gmail.com>
2015-05-20 13:59:11 -0400

    Update example.rb

--- example.rb
+++ example.rb
@@ -1,5 +1,5 @@
 puts 'hello world!'
 puts 'Hello the second time!'
-puts "change after someone's pull request"
+puts "my change should conflict with yours"
 puts 'Hi from Batman'
 puts "Hi again from Batman in double quotes"
