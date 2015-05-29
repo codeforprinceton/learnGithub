07acf958c6a3deed61d0aa365e9075ae61b7222b
Ben Mathew <beneh.mathew@gmail.com>
2015-05-29 18:37:56 -0400

    Testing GitHub for Mac Syncing

--- example.rb
+++ example.rb
@@ -2,4 +2,4 @@ puts 'hello world!'
 puts 'Hello the second time!'
 puts "fixed conflict"
 puts 'Hi from Batman'
-puts "Hi again from Batman in double quotes"
+puts "Hi again from Batman with double quotes"
